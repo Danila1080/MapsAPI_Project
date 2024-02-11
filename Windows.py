cf6b79e1ca3d29056cacade0fbe1d5ceeb4f8b82
Danila1080 <109276687+Danila1080@users.noreply.github.com>
2024-02-11 14:51:30 +0300

    Сделал изменение мастаба по нажатию клавиш pgup и pgdn, также работают стрелки. !Бывает задержка. Иногда, чтобы изменилось, нужно кликнуть несколько раз!

--- Windows.py
+++ Windows.py
@@ -2,6 +2,7 @@ import os
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtCore import Qt
 from finder import get_image
 
 
@@ -14,12 +15,41 @@ class MainWindow(QMainWindow):
         self.setStyleSheet("background-color: #FFCF48")
         self.Header.setFont(QFont('', 15))
 
-        # получение изображения карты и запись в файл
-        self.maps_image = get_image('Москва')
+        # объявление пустых переменных
+        self.current_spn = None
+        self.current_address = None
+        self.maps_image = None
+
+        # получение первых данных
+        self.get_data('Москва')
+
+    def get_data(self, address, spn_custom_value=None):
+        # получение данных
+        data = get_image(address, spn_custom_value)
+        self.maps_image = data[0]
+        self.current_spn = data[1]
+        self.current_address = address
+
+        # запись в файл
         with open('data/pictures/img.png', mode='wb') as img:
             img.write(self.maps_image.content)
 
         # применение картики из файла к label-виджету
         self.Map.setPixmap(QPixmap('data/pictures/img.png'))
 
+        # стирание файла
         os.remove('data/pictures/img.png')
+
+    # обработка кликов
+    def keyPressEvent(self, event):
+        new_current_spn = None
+
+        if event.key() == Qt.Key_Up or event.key() == Qt.Key_PageUp:
+            new_current_spn = [(x - 0.05) for x in self.current_spn]
+        if event.key() == Qt.Key_Down or event.key() == Qt.Key_PageDown:
+            new_current_spn = [(x + 0.05) for x in self.current_spn]
+
+        # проверка ограничений
+        if 0 < new_current_spn[0] < 90 and 0 < new_current_spn[1] < 90:
+            self.current_spn = new_current_spn
+            self.get_data(self.current_address, self.current_spn)
--- finder.py
+++ finder.py
@@ -2,27 +2,37 @@ from geocoder import get_ll_span
 import requests
 
 
-def get_image(toponym, spn_value=None):
+def get_image(toponym, spn_custom_value=None):
     '''
     функция получает изображение по запросу,
-    возващает поток байт, либо false, в качестве аргумента принимает адрес
+    возващает поток байт и масштаб, либо false. В качестве аргумента принимает адрес
     и необязательный аргумент spn_value.
     :param toponym:
-    :param spn_value:
-    :return response, False:
+    :param spn_custom_value:
+    :return response, spn_to_return:
+    :return False:
     '''
     if toponym:
         ll, spn = get_ll_span(toponym)
+        # текущее значение
+        spn_to_return = [float(x) for x in spn.split(',')]
+
+        # параметры запроса
         map_params = {
             "ll": ll,
             "spn": spn,
             "l": "map",
         }
-        # если передан масштаб, то соответствующий параметр в запросе будет изменен
-        if spn_value:
-            map_params["spn"] = str(','.join(str(x) for x in spn_value))
 
+        # если передан масштаб, то параметр для запроса будет изменен
+        if spn_custom_value:
+            map_params["spn"] = str(','.join(str(x) for x in spn_custom_value))
+            spn_to_return = spn_custom_value
+
+        # запрос
         map_api_server = "http://static-maps.yandex.ru/1.x/"
         response = requests.get(map_api_server, params=map_params)
-        return response
+
+        return response, spn_to_return
+
     return False
